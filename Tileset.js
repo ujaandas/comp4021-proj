4d16db1ca65bee6dd3e87407363837187b5ae5ee
Ujaan Das <ujaandas03@gmail.com>
2025-04-25 22:49:20 +0800

    we do a bit of obfuscation

--- Tileset.js
+++ Tileset.js
@@ -6,7 +6,7 @@ class Tileset {
 
         for (let i = 0; i < w; i++) {
             for (let j = 0; j < h; j++) {
-                this.addPt(i, j);
+                this._addPt(i, j);
             }
         }
     }
@@ -15,24 +15,24 @@ class Tileset {
         return `${i},${j}`;
     }
 
-    addPt(i, j) {
+    _addPt(i, j) {
         const key = this._createKey(i, j);
         if (!this.adj.has(key)) {
             this.adj.set(key, []);
             console.log(`Added point ${i}, ${j}`);
 
-            const neighbors = [
-                [i - 1, j], // left
-                [i + 1, j], // right
-                [i, j - 1], // top
-                [i, j + 1]  // bottom
+            const n = [
+                [i - 1, j], // top left
+                [i, j - 1], // top right
+                [i + 1, j], // bottom right
+                [i, j + 1]  // bottom left
             ];
 
-            for (const [ni, nj] of neighbors) {
-                const neighborKey = this._createKey(ni, nj);
-                if (this.adj.has(neighborKey)) {
+            for (const [ni, nj] of n) {
+                const nKey = this._createKey(ni, nj);
+                if (this.adj.has(nKey)) {
                     this.adj.get(key).push({ i: ni, j: nj });
-                    this.adj.get(neighborKey).push({ i: i, j: j });
+                    this.adj.get(nKey).push({ i: i, j: j });
                     console.log(`Added edge between (${i}, ${j}) and (${ni}, ${nj})`);
                 }
             }
